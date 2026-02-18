c11ba4638a265c89bbc3cd6459c7c1f8f3fabb98
EduSantos <eduardofelipi@gmail.com>
2026-02-18 13:36:50 -0300

    chore: bump app version

--- app.config.ts
+++ app.config.ts
@@ -64,7 +64,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     name: isDevVariant ? 'Finn Dev' : 'Finn',
     slug: isDevVariant ? 'finn-dev' : 'finn',
-    version: '2.0.0',
+    version: '2.1.0',
     orientation: 'portrait',
     scheme: isDevVariant ? 'finn-dev' : 'finn',
     icon: './assets/icon.png',
